c3ad940f578552289d4220ffe1d30093dea4f9f6
Mahabub 175 <rgrxmahabub.official@gmail.com>
2024-12-25 12:44:05 +0600

    updated

--- src/app/(landingLayout)/layout.js
+++ src/app/(landingLayout)/layout.js
@@ -2,7 +2,6 @@ import BackToTop from "@/components/Shared/BackToTop";
 import LandingFooter from "@/components/Shared/Footer/LandingFooter";
 import LandingHeader from "@/components/Shared/Navbar/LandingHeader";
 import GlobalCart from "@/components/Shared/Product/GlobalCart";
-import Script from "next/script";
 
 const LandingLayout = ({ children }) => {
   return (
@@ -11,22 +10,6 @@ const LandingLayout = ({ children }) => {
       <GlobalCart />
       {children}
       <BackToTop />
-      <Script
-        id="crisp-chat"
-        strategy="lazyOnload"
-        dangerouslySetInnerHTML={{
-          __html: `
-              window.$crisp=[];window.CRISP_WEBSITE_ID="a716db7a-951a-48d0-823a-79141fe8b539";
-              (function(){
-                d=document;
-                s=d.createElement("script");
-                s.src="https://client.crisp.chat/l.js";
-                s.async=1;
-                d.getElementsByTagName("head")[0].appendChild(s);
-              })();
-            `,
-        }}
-      />
       <LandingFooter />
     </>
   );
