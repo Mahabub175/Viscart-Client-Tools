bfab81c824b3543d2f3bb4a4f583f862346db00d
Mahabub 175 <rgrxmahabub.official@gmail.com>
2024-12-30 17:40:09 +0600

    updated

--- src/utilities/configs/base_api.js
+++ src/utilities/configs/base_api.js
@@ -1,5 +1,5 @@
-export const base_url = "https://viscartdemoapi.vitasoftsolutions.com/api/v1";
-export const base_url_image = "https://viscartdemoapi.vitasoftsolutions.com/";
+export const base_url = "https://viscartapi.moonsgallerysystem.com/api/v1";
+export const base_url_image = "https://viscartapi.moonsgallerysystem.com/";
 // export const base_url = "http://localhost:5000/api/v1";
 // export const base_url_image = "http://localhost:5000/";
 // export const base_url = "http://192.168.1.6:5000/api/v1";
