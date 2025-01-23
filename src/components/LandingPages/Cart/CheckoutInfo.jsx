176d9913238579191eecaf58ca6a79caf42f3283
Astro <rgrxmahabub.official@gmail.com>
2025-01-23 21:49:53 +0600

    payment method updated

--- src/components/LandingPages/Cart/CheckoutInfo.jsx
+++ src/components/LandingPages/Cart/CheckoutInfo.jsx
@@ -5,16 +5,34 @@ import { useGetAllGlobalSettingQuery } from "@/redux/services/globalSetting/glob
 
 const CheckoutInfo = () => {
   const form = Form.useFormInstance();
-  const paymentType = Form.useWatch("paymentType", form);
-  const paymentMethod = Form.useWatch("paymentMethod", form);
+  const selectedPayment = Form.useWatch("paymentMethod", form);
 
   const { data: globalData } = useGetAllGlobalSettingQuery();
 
   const paymentOptions = [
-    { value: "manual", label: "Manual" },
-    { value: "cod", label: "Cash on Delivery" },
+    {
+      value: "bkash",
+      label: "Bkash",
+      info: "Bkash Information: Please send payment to 01XXXXXXXXX.",
+    },
+    {
+      value: "nagad",
+      label: "Nagad",
+      info: "Nagad Information: Please send payment to 01XXXXXXXXX.",
+    },
+    {
+      value: "cod",
+      label: "Cash on Delivery",
+      info: "Cash on Delivery Information: Please make sure to have the exact amount ready during delivery.",
+    },
     ...(globalData?.results?.ssl === "Active"
-      ? [{ value: "ssl", label: "SSL Commerz" }]
+      ? [
+          {
+            value: "ssl",
+            label: "SSL Commerz",
+            info: "SSL Commerz: You will be redirected to the SSL Commerz payment gateway.",
+          },
+        ]
       : []),
   ];
 
@@ -24,77 +42,20 @@ const CheckoutInfo = () => {
       <CustomInput type="number" name="number" label="Number" required />
       <CustomInput type="textarea" name="address" label="Address" required />
 
-      <Form.Item name="paymentType" label="Payment Type" required>
-        <Radio.Group>
+      <Form.Item name="paymentMethod" label="Payment Method" required>
+        <Radio.Group className="flex flex-col gap-2">
           {paymentOptions.map((option) => (
-            <Radio key={option.value} value={option.value}>
-              {option.label}
-            </Radio>
+            <div key={option.value} className="flex flex-col">
+              <Radio value={option.value}>{option.label}</Radio>
+              {selectedPayment === option.value && (
+                <p className="mt-1 pl-6 text-sm text-primary font-semibold">
+                  {option.info}
+                </p>
+              )}
+            </div>
           ))}
         </Radio.Group>
       </Form.Item>
-
-      {paymentType === "manual" && (
-        <div>
-          <Form.Item name="paymentMethod" label="Payment Method" required>
-            <Radio.Group>
-              <Radio value="bkash">Bkash</Radio>
-              <Radio value="nagad">Nagad</Radio>
-              <Radio value="rocket">Rocket</Radio>
-              <Radio value="upay">Upay</Radio>
-            </Radio.Group>
-          </Form.Item>
-
-          {paymentMethod && (
-            <div className="mt-4">
-              {paymentMethod === "bkash" && (
-                <div className="mb-4">
-                  <p>Bkash Information: Please send payment to 01XXXXXXXXX.</p>
-                </div>
-              )}
-              {paymentMethod === "nagad" && (
-                <div className="mb-4">
-                  <p>Nagad Information: Please send payment to 01XXXXXXXXX.</p>
-                </div>
-              )}
-              {paymentMethod === "rocket" && (
-                <div className="mb-4">
-                  <p>Rocket Information: Please send payment to 01XXXXXXXXX.</p>
-                </div>
-              )}
-              {paymentMethod === "upay" && (
-                <div className="mb-4">
-                  <p>Upay Information: Please send payment to 01XXXXXXXXX.</p>
-                </div>
-              )}
-              <CustomInput
-                type="text"
-                name="tranId"
-                label="Transaction ID"
-                required
-              />
-            </div>
-          )}
-        </div>
-      )}
-
-      {paymentType === "cod" && (
-        <div className="mb-4 -mt-4">
-          <p>
-            Cash on Delivery Information: Please make sure to have the exact
-            amount ready during delivery.
-          </p>
-        </div>
-      )}
-
-      {paymentType === "ssl" && (
-        <div className="my-4">
-          <p>
-            SSL Commerz: You will be redirected to the SSL Commerz payment
-            gateway.
-          </p>
-        </div>
-      )}
 
       <SubmitButton fullWidth text="Order Now" />
     </div>
