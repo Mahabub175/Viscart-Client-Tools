bf3a46f45f64fd4a66a8747929af67036b849943
Astro <rgrxmahabub.official@gmail.com>
2025-01-28 02:18:16 +0600

    updated

--- src/app/(landingLayout)/layout.js
+++ src/app/(landingLayout)/layout.js
@@ -7,7 +7,7 @@ const LandingLayout = ({ children }) => {
   return (
     <>
       <LandingHeader />
-      <div className="mt-[5.7rem] md:mt-[6.4rem] lg:mt-[10.9rem]">
+      <div className="mt-[5.7rem] md:mt-[6.4rem] lg:mt-[11.4rem] xl:mt-[9.8rem] xxl:mt-[10.5rem]">
         {children}
       </div>
       <BackToTop />
