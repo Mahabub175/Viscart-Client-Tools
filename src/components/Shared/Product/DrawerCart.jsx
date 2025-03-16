c96842f43bfe49097d79e75d74d1631e7ff5333b
Mahabub175 <rgrxmahabub.official@gmail.com>
2025-03-16 16:02:15 +0600

    updated

--- src/components/Shared/Product/DrawerCart.jsx
+++ src/components/Shared/Product/DrawerCart.jsx
@@ -13,16 +13,54 @@ import { useEffect, useMemo, useState } from "react";
 import { toast } from "sonner";
 import { MdDelete } from "react-icons/md";
 import { useRouter } from "next/navigation";
+import { sendGTMEvent } from "@next/third-parties/google";
+import { useSelector } from "react-redux";
+import { useDeviceId } from "@/redux/services/device/deviceSlice";
+import { useCurrentUser } from "@/redux/services/auth/authSlice";
+import { useGetSingleUserQuery } from "@/redux/services/auth/authApi";
 
 const DrawerCart = ({ data, setDrawer }) => {
+  const deviceId = useSelector(useDeviceId);
+  const user = useSelector(useCurrentUser);
+
+  const { data: userData } = useGetSingleUserQuery(user?._id, {
+    skip: !user?._id,
+  });
+
   const [counts, setCounts] = useState({});
   const { data: globalData } = useGetAllGlobalSettingQuery();
   const [deleteCart] = useDeleteCartMutation();
   const [updateCart] = useUpdateCartMutation();
+
   const router = useRouter();
+
+  const proceedToCheckout = () => {
+    router.push("/cart");
+    setDrawer(false);
+  };
 
   useEffect(() => {
     if (data?.length) {
+      const productData = data?.map((item) => {
+        return {
+          id: item._id,
+          productId: item.productId,
+          productName: item.productName,
+          price: item.price,
+          sku: item.sku,
+          slug: item.slug,
+          quantity: item.quantity,
+          image: formatImagePath(item.image),
+          ...(user?._id
+            ? {
+                userName: userData?.name,
+                userNumber: userData?.number,
+                userEmail: userData?.email,
+              }
+            : { deviceId }),
+        };
+      });
+      sendGTMEvent({ event: "cartView", value: productData });
       setCounts(
         data.reduce(
           (acc, item) => ({
@@ -33,7 +71,7 @@ const DrawerCart = ({ data, setDrawer }) => {
         )
       );
     }
-  }, [data]);
+  }, [data, user?._id, userData, deviceId]);
 
   const subtotal = useMemo(() => {
     return data?.reduce((total, item) => {
@@ -69,11 +107,6 @@ const DrawerCart = ({ data, setDrawer }) => {
     }
   };
 
-  const proceedToCheckout = () => {
-    router.push("/cart");
-    setDrawer(false);
-  };
-
   return (
     <div>
       {data?.length ? (
@@ -81,75 +114,65 @@ const DrawerCart = ({ data, setDrawer }) => {
           {data.map((item) => (
             <div
               key={item._id}
-              className="flex flex-col lg:flex-row items-center gap-4 justify-between pb-5 mt-5 first:mt-0 border-b border-gray-300 last:border-b-0"
+              className="flex items-center gap-4 pb-5 mt-5 first:mt-0 border-b border-gray-300 last:border-b-0"
             >
-              <div className="flex items-center gap-4 relative group flex-[3]">
-                <div className="relative">
-                  <Image
-                    src={formatImagePath(item.image)}
-                    alt={item.product?.name || "Product Image"}
-                    width={128}
-                    height={128}
-                    className="lg:w-[150px] lg:h-32 rounded-xl"
-                  />
-                </div>
-                <div className="w-full">
-                  <Link
-                    href={`/products/${item.slug}`}
-                    className="text-base font-normal hover:underline hover:text-black"
-                    onClick={() => setDrawer(false)}
-                  >
-                    <Tooltip placement="top" title={item.productName}>
-                      <h2 className="text-md font-semibold">
-                        {item.productName}
-                      </h2>
-                    </Tooltip>
-                    {item.variant &&
-                      ` (${item.variant.attributeCombination
-                        .map((comb) => comb.name)
-                        .join(", ")})`}
-                  </Link>
-
-                  <div className="flex justify-between items-start">
-                    <div className="mt-2">
-                      <p className="font-semibold">
-                        Quantity: {counts[item._id]}
-                      </p>
-                      <div className="flex items-center border rounded w-fit my-2">
-                        <button
-                          onClick={() =>
-                            handleQuantityChange(item._id, counts[item._id] - 1)
-                          }
-                          className="px-2 py-1 bg-gray-200 text-sm font-bold"
-                          disabled={counts[item._id] <= 1}
-                        >
-                          -
-                        </button>
-                        <span className="px-4">{counts[item._id]}</span>
-                        <button
-                          onClick={() =>
-                            handleQuantityChange(item._id, counts[item._id] + 1)
-                          }
-                          className="px-2 py-1 bg-gray-200 text-sm font-bold"
-                        >
-                          +
-                        </button>
-                      </div>
-                    </div>
-                    <MdDelete
-                      onClick={() => handleDeleteCart(item._id)}
-                      className="text-xl cursor-pointer mt-2 hover:scale-105 duration-300"
-                    />
-                  </div>
-                  <div>
-                    <p className="text-primary text-xl font-bold">
-                      {globalData?.results?.currency +
-                        " " +
-                        (item.price * counts[item._id]).toFixed(2)}
-                    </p>
-                  </div>
-                </div>
+              <div className="border rounded-xl">
+                <Image
+                  src={formatImagePath(item.image)}
+                  alt={item.product?.name || "Product Image"}
+                  width={128}
+                  height={128}
+                  className="lg:w-[120px] lg:h-32 rounded-xl"
+                />
               </div>
+
+              <div className="flex-1 space-y-2">
+                <Link
+                  href={`/products/${item.slug}`}
+                  className="text-base font-normal hover:underline hover:text-black"
+                  onClick={() => setDrawer(false)}
+                >
+                  <Tooltip placement="top" title={item.productName}>
+                    <h2 className="text-md font-semibold">
+                      {item.productName}
+                    </h2>
+                  </Tooltip>
+                  {item.variant &&
+                    ` (${item.variant.attributeCombination
+                      .map((comb) => comb.name)
+                      .join(", ")})`}
+                </Link>
+                <div className="flex items-center border rounded w-fit">
+                  <button
+                    onClick={() =>
+                      handleQuantityChange(item._id, counts[item._id] - 1)
+                    }
+                    className="px-2 py-1 bg-gray-200 text-sm font-bold"
+                    disabled={counts[item._id] <= 1}
+                  >
+                    -
+                  </button>
+                  <span className="px-4">{counts[item._id]}</span>
+                  <button
+                    onClick={() =>
+                      handleQuantityChange(item._id, counts[item._id] + 1)
+                    }
+                    className="px-2 py-1 bg-gray-200 text-sm font-bold"
+                  >
+                    +
+                  </button>
+                </div>
+                <p className="text-primary text-xl font-bold">
+                  {globalData?.results?.currency +
+                    " " +
+                    (item.price * counts[item._id]).toFixed(2)}
+                </p>
+              </div>
+
+              <MdDelete
+                onClick={() => handleDeleteCart(item._id)}
+                className="text-xl cursor-pointer hover:scale-105 duration-300"
+              />
             </div>
           ))}
         </div>
